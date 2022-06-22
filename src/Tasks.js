06bce368af7fd73890c13808dfe6d4106fa6cbd2
Ozkar Alvarez <ozkaralvarez98@hotmail.com>
2022-06-22 11:37:04 -0700

    fully functional logic

--- src/Tasks.js
+++ src/Tasks.js
@@ -1,6 +1,8 @@
 import React from "react";
 import isToday from "date-fns/isToday";
 import parseISO from "date-fns/parseISO";
+import addDays from "date-fns/addDays";
+import isBefore from "date-fns/isBefore";
 
 export default function Tasks(props) {
   const allProjTasks = [];
@@ -22,10 +24,44 @@ export default function Tasks(props) {
     );
   } else if (props.selectedFilter.filter === "Today") {
     let todayTasks = allTasks.filter((task) => isToday(parseISO(task.date)));
-
+    console.log(parseISO("2022-06-15"));
     return (
       <ul>
         {todayTasks.map((task) => (
+          <li key={task.id}>
+            {task.title}
+            <div>{task.date}</div>
+          </li>
+        ))}
+      </ul>
+    );
+  } else if (props.selectedFilter.filter === "Week") {
+    let weekFromToday = addDays(new Date(), 7);
+    let weekTasks = allTasks.filter((task) =>
+      isBefore(parseISO(task.date), weekFromToday)
+    );
+
+    return (
+      <ul>
+        {weekTasks.map((task) => (
+          <li key={task.id}>
+            {task.title}
+            <div>{task.date}</div>
+          </li>
+        ))}
+      </ul>
+    );
+  } else {
+    const allSelectedProjTasks = [];
+    props.projects.forEach((proj) => {
+      if (proj.id === props.selectedFilter.projectId) {
+        allSelectedProjTasks.push(...proj.tasks);
+      }
+    });
+
+    return (
+      <ul>
+        {allSelectedProjTasks.map((task) => (
           <li key={task.id}>
             {task.title}
             <div>{task.date}</div>
