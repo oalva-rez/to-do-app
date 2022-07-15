1737387f2f00879e39c7d40706db87bcc1f72100
Ozkar Alvarez <ozkaralvarez98@hotmail.com>
2022-07-15 11:58:41 -0700

    css merge

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import "./components/SignIn/signin.css";
 import "./components/SignUp/signup.css";
+import "./components/Greeting/greeting.css";
+import "./components/Tasks/tasks.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
